6a7e2a39ff8707b73447096650708dca3d649ca5
nwauford <noreen@mit.edu>
2023-06-21 16:48:08 -0400

    SI figs 25 & 29

---
+++ Figure 4/Supplementary Figure 29/analysis_images/rep3/threshold_no_transitions_rep3.m
@@ -0,0 +1,59 @@
+numClusters = zeros(64,2);
+if ~exist('data_no_transitions_r3.mat')
+    for i = 1:64
+        
+            I = imread(['r_images/sim_' num2str(i + 127) '_cropped_1.png']);
+        
+%         figure('position',[31 230 643 561]);  
+%         imshow(I);
+        I = rgb2gray(I);
+        BW = imbinarize(I, .07);
+        Icropped = imcrop(BW, [0 0 1360 880]);
+        BW2 = bwareafilt(Icropped,[1200 Inf]);
+        [labeled, num] = bwlabel(BW2);
+        numClusters(i, 1) = num;
+%         figure('position',[697 227 708 554]);
+%         imshow(labeled)
+
+        imwrite( labeled, ['clustered_r_' num2str(i) '.png'])
+
+        I = imread(['y_images/sim_' num2str(i + 127) '_cropped_2.png']);
+%         figure('position',[31 230 643 561]);  
+%         imshow(I);
+        I = rgb2gray(I);
+        BW = imbinarize(I, .165);
+        Icropped = imcrop(BW, [0 0 1360 880]);
+        BW2 = bwareafilt(Icropped,[600 Inf]);
+        [labeled, num] = bwlabel(BW2);
+        numClusters(i, 2) = num;
+%         figure('position',[697 227 708 554]);  
+%         imshow(labeled)
+
+        imwrite( labeled, ['clustered_y_' num2str(i) '.png'])
+
+    end
+    save('data_no_transitions_r3.mat', 'numClusters')
+else
+    load('data_no_transitions_r3.mat')
+end
+
+ redArr = reshape(numClusters(:, 1), [8 8])
+ redArr = redArr';
+redArr = redArr .* (256 / max(numClusters(:, 1))); 
+yArr = reshape(numClusters(:, 2), [8 8]);
+yArr = yArr';
+yArr = yArr .* (256 / max(numClusters(:, 2))); 
+
+figure;
+image(redArr)
+colormap viridis
+caxis([0 5])
+title('Red cluster numbers');
+
+figure;
+image(yArr)
+caxis([0 5])
+colormap viridis
+title('Yellow cluster numbers');
+
+numClusters
---
+++ Figure 4/Supplementary Figure 29/clusterStats_no_transitions.m
@@ -0,0 +1,129 @@
+% {intensity_threshold, channel filename character}
+y_param = {0.12, 'y'};
+r_param = {0.07, 'r'};
+area_thresh = 400;
+num_doses = 64; 
+
+usrC = strsplit(pwd,'/');
+fex_code_path = ['/Users/' usrC{3} '/Documents/MIT/Weiss Lab/microscopy/cbrewer'];
+addpath(genpath(fex_code_path))
+
+dirs = {'analysis_images/rep1/', 'analysis_images/rep2/', 'analysis_images/rep3/'};
+
+if ~exist('data_no_transitions.mat')
+        out_y = clust_stats(y_param, num_doses, area_thresh, dirs);
+        out_r = clust_stats(r_param, num_doses, area_thresh, dirs);
+        data_no_transitions = {out_y, out_r};
+        save('data_no_transitions','data_no_transitions')
+else 
+    load('data_no_transitions.mat')
+    out_y = data{1};
+    out_r = data{2};
+end
+
+numClustArray = out_y{1};
+    areaClustArray = out_y{2};
+    
+    % scale area to convert between simulated & experimental images
+    % since each pixel is 5 uM, multiply by 5^2
+    areaClustArray = areaClustArray .* 25 ;
+    
+    % show heat maps of array with image(<Array>)
+    fig = figure('Position', [525 1 563 804]);
+    t = tiledlayout(2,1,'TileSpacing','Compact');
+    title(t, ['Yellow no cell state transitions'])
+   
+    nexttile;
+    h = heatmap(numClustArray, 'CellLabelColor','none');
+    h.GridVisible = 'off';
+    colormap viridis
+    title("Avg. Number Clusters")
+    caxis([0 5])
+    colorbar
+    
+    nexttile;
+    h = heatmap(areaClustArray, 'CellLabelColor','none');
+    h.GridVisible = 'off';
+    colormap viridis
+    title("Avg. Area Clusters (sq. uM)")
+    caxis([0 5e4])
+    colorbar
+
+    saveas(t, "Y-heatmaps-no_transitions.png");
+    
+    numClustArray = out_r{1};
+    areaClustArray = out_r{2};
+    
+    % scale area to convert between simulated & experimental images
+    % since each pixel is 5 uM, multiply by 5^2
+    areaClustArray = areaClustArray .* 25 ;
+    
+    fig = figure('Position', [525 1 563 804]);
+    t = tiledlayout(2,1,'TileSpacing','Compact');
+    title(t, ['Red no cell state transitions'])
+    
+    nexttile;
+    h = heatmap(numClustArray, 'CellLabelColor','none');
+    colormap viridis
+    h.GridVisible = 'off';
+    title("Avg. Number Clusters")
+    caxis([0 5])
+    colorbar
+    
+    nexttile;
+    h = heatmap(areaClustArray, 'CellLabelColor','none');
+    colormap viridis
+    h.GridVisible = 'off';
+    title("Avg. Area Clusters (sq. uM)")
+    caxis([0 5e4])
+    colorbar
+
+    saveas(t, "R-heatmaps-no_transitions.png");
+
+
+function out = clust_stats(chan_param, num_doses, area_thresh, dirs)
+    intens_tresh = chan_param{1,1};
+    chan = chan_param{1,2};
+    
+    % data structures formatted as 64 wells X channel
+
+    [numClusters, areaClusters, malClusters] = deal(zeros(64,1));
+    
+    
+    for d = 1:length(dirs)
+       
+        %parpool(3);
+        for f = 1:num_doses
+            
+            fName = [dirs{d} 'clustered_' chan '_' num2str(f) '.png'];
+            fprintf("Processing %s \n", fName)
+
+            I = imread(fName);
+
+            [labeled, num] = bwlabel(I);
+
+            numClusters(f, 1) = numClusters(f, 1) + num;
+            stats = regionprops(I, "Area", "MajorAxisLength");
+            areaClusters(f, 1) = areaClusters(f, 1) + sum([stats(:).Area]).*.62;
+
+           
+            
+        end
+    end
+
+    avg_areaClusters = areaClusters./numClusters;
+    avg_malClusters = malClusters./numClusters;
+
+    idx = 1;
+    numClustArray = reshape(numClusters(:, idx), [8 8])' ./ 3;
+   
+    areaClustArray = reshape(avg_areaClusters(:, idx), [8 8])' ./ 3;
+    areaClustArray(isnan(areaClustArray)) = 0;
+    
+    out{1} = numClustArray;
+    out{2} = areaClustArray;
+end
+    
+    
+
+
